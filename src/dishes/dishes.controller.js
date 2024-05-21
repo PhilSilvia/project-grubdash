a9912950120401861a6ff0d95b9b2e321824c449
Phillip Silvia <wizcom86@gmail.com>
2024-05-21 11:00:33 -0700

    Validation handlers added to POST /dishes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -8,15 +8,39 @@ const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
 function list(req, res, next){
+    res.json({ data: dishes });
+}
 
+function bodyDataHas(propertyName){
+    return function(req, res, next){
+        const { data = {} } = req.body;
+        if (data[propertyName] && data[propertyName] !== ""){
+            return next();
+        }
+        next({
+            status: 400,
+            message: `Dish must include a ${propertyName}`
+        });
+    };
+}
+
+function priceIsValid(req, res, next){
+    const { data: { price } = {} } = req.body;
+    if (price && Number.isInteger(price) && price > 0){
+        return next();
+    }
+    next({ 
+        status: 400,
+        message: `Dish must have a price that is an integer greater than 0`
+    });
 }
 
 function create(req, res, next){
-
+    res.json({ data: req.body.data });
 }
 
 function dishExists(req, res, next){
-    
+
 }
 
 function read(req, res, next){
@@ -29,7 +53,21 @@ function update(req, res, next){
 
 module.exports = {
     list,
-    create: [ create ],
+    create: [ 
+        bodyDataHas("name"), 
+        bodyDataHas("description"), 
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        priceIsValid,
+        create 
+    ],
     read: [ dishExists, read ],
-    update: [ dishExists, update ],
+    update: [ 
+        dishExists, 
+        bodyDataHas("name"), 
+        bodyDataHas("description"), 
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        priceIsValid,
+        update ],
 };
