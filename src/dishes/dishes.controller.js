9e9daf476cbbc9891e648802eee7acb9d1068629
Phillip Silvia <wizcom86@gmail.com>
2024-05-21 11:54:31 -0700

    Added comments to the router and controller files

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,17 +6,24 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
+// List function for /orders, responds with the current list of dishes
 function list(req, res, next){
     res.json({ data: dishes });
 }
 
+// Validator that returns a function that checks for the existence of a given property and ensures it is
+// not an empty string
 function bodyDataHas(propertyName){
+    // Return the validation function
     return function(req, res, next){
+        // Retrieves the data from the request body
         const { data = {} } = req.body;
+        // Checks to see if the given property exists and is not an empty string
         if (data[propertyName] && data[propertyName] !== ""){
+            // If it's valid, move on to the next middleware function
             return next();
         }
+        // If its invalid, stop and send an error to the user
         next({
             status: 400,
             message: `Dish must include a ${propertyName}`
@@ -24,19 +31,27 @@ function bodyDataHas(propertyName){
     };
 }
 
+// Validation function for the price, which has some extra stipulations.
+// Ensures the price is a non-zero, non-negative integer
 function priceIsValid(req, res, next){
+    // Retrieve the price given in the request body
     const { data: { price } = {} } = req.body;
+    // If the price exists, is an integer, and is greater than 0, we move onto the next middleware function
     if (price && Number.isInteger(price) && price > 0){
         return next();
     }
+    // If the price is invalid, we respond with an error message for the user
     next({ 
         status: 400,
         message: `Dish must have a price that is an integer greater than 0`
     });
 }
 
+// Creates a new dish with the given properties. Called after all validation has been passed. 
 function create(req, res, next){
+    // Retrieve the properties from the request body
     const { data: { name, description, price, image_url } = {} } = req.body;
+    // Create a new dish with the properties from the request and a new id
     const newDish = {
         id: nextId(),
         name,
@@ -44,28 +59,40 @@ function create(req, res, next){
         price, 
         image_url,
     };
+    // Adds the dish to our list of dishes
     dishes.push(newDish);
+    // Responds with the new dish after successfully creating the new dish
     res.status(201).json({ data: newDish });
 }
 
+// Validation function to ensure that a dish exists for a given id
 function dishExists(req, res, next){
+    // Retrieve the dish id from the route parameters
     const { dishId } = req.params;
+    // Locate the dish from our list given the id
     const foundDish = dishes.find((dish) => dish.id === dishId);
+    // If we successfully matched the id and found a dish...
     if (foundDish){
+        // Set the local paramter to the found dish so we don't have to search again
         res.locals.dish = foundDish;
+        // Move onto the next middleware function
         return next();
     }
+    // Otherwise, we return an error message, since we couldn't find a matching dish
     next({
         status: 404,
         message: `Dish does not exist: ${dishId}`
     })
 }
 
+// Returns the data for a specified dish based on its id. Called after validating that the dish exists.
 function read(req, res, next){
     const dish = res.locals.dish;
     res.json({ data: dish });
 }
 
+// Updates a given dish with new properties. Called after validating the given properties and 
+// that the dish exists. 
 function update(req, res, next){
     const dish = res.locals.dish;
     const { data: { name, description, price, image_url } = {} } = req.body;
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,17 +7,24 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 const { del } = require("express/lib/application");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+// Responds with the current list of orders
 function list(req, res, next){
     res.json({ data: orders });
 }
 
+// Validator that returns a function that checks for the existence of a given property and ensures it is
+// not an empty string
 function bodyDataHas(propertyName){
+    // Returns a validator function
     return function(req, res, next){
+        // Retrieves the data from the request body
         const { data = {} } = req.body;
+        // Checks to see if the property exists and is not an empty string
         if (data[propertyName] && data[propertyName] !== ""){
+            // If its valid, move onto the next middleware function
             return next();
         }
+        // If its invalid, respond with an error message for the user
         next({
             status: 400,
             message: `Order must include a ${propertyName}`
@@ -25,6 +32,8 @@ function bodyDataHas(propertyName){
     };
 }
 
+// Validation function for the dishes property, since it needs to be an array of dishes and each dish
+// needs to have a quantity greater than 0
 function dishesIsValid(req, res, next){
     const { data: { dishes } = {} } = req.body;
     if (dishes && Array.isArray(dishes) && dishes.length > 0){
@@ -44,8 +53,11 @@ function dishesIsValid(req, res, next){
     });
 }
 
+// Creates a new order with the given properties after passing our validation checks
 function create(req, res, next){
+    // Retrieve the properties from the request body
     const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+    // Create a new order with the given properties
     const newOrder = {
         id: nextId(),
         deliverTo,
@@ -53,22 +65,31 @@ function create(req, res, next){
         status,
         dishes
     };
+    // Add the new order to our list of orders
     orders.push(newOrder);
+    // Respond with the new order details
     res.status(201).json({ data: newOrder });
 }
 
+// Validation function that ensures an order exists for a given id
 function orderExists(req, res, next){
 
 }
 
+// Responds with the details for a given order id. Called after validating that
+// an order exists with the given id
 function read(req, res, next){
 
 }
 
+// Updates the order of a given id with new properties. Called after validating the given properties
+// and that an order exists at the given id
 function update(req, res, next){
 
 }
 
+// Deletes the entry for an order at a given id. Called after validating that an order exists at the
+// given id
 function destroy(req, res, next){
 
 }
