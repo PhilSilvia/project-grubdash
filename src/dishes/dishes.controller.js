06fd6657f083598312d7605ae1331ab5a38f9dbe
Phillip Silvia <wizcom86@gmail.com>
2024-05-21 10:42:37 -0700

    Skeleton of controller functions and routes set up

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,29 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function list(req, res, next){
+
+}
+
+function create(req, res, next){
+
+}
+
+function dishExists(req, res, next){
+    
+}
+
+function read(req, res, next){
+
+}
+
+function update(req, res, next){
+
+}
+
+module.exports = {
+    list,
+    create: [ create ],
+    read: [ dishExists, read ],
+    update: [ dishExists, update ],
+};
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,34 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+function list(req, res, next){
+
+}
+
+function create(req, res, next){
+
+}
+
+function orderExists(req, res, next){
+    
+}
+
+function read(req, res, next){
+
+}
+
+function update(req, res, next){
+
+}
+
+function destroy(req, res, next){
+
+}
+
+module.exports = {
+    list,
+    create: [ create ],
+    read: [ orderExists, read ],
+    update: [ orderExists, update ],
+    delete: [ orderExists, destroy ],
+};
